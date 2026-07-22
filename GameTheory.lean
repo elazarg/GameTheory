d6abaccb39a92a1fa1dd7f04625730b53d741921
Elazar Gershuni <elazarg@gmail.com>
2026-07-22 11:25:17 +0300

    Prove Fink discounted stationary equilibrium

--- GameTheory.lean
+++ GameTheory.lean
@@ -113,6 +113,7 @@ import GameTheory.Concepts.Stochastic.Absorbing
 import GameTheory.Concepts.Stochastic.TransitionIndependent
 import GameTheory.Concepts.Stochastic.Discounted
 import GameTheory.Concepts.Stochastic.ZeroSum
+import GameTheory.Concepts.Stochastic.Fink
 
 -- Correlated equilibrium
 import GameTheory.Concepts.Correlation.CorrelatedEqProperties
