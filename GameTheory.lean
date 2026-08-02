a1e6f4aceaed2cfe3f9a5e8ea2b2bdc22933d2af
Elazar Gershuni <elazarg@gmail.com>
2026-08-02 18:02:49 +0300

    Classify arbitrary sure-first quitting profiles

--- GameTheory.lean
+++ GameTheory.lean
@@ -418,6 +418,7 @@ import GameTheory.Concepts.Stochastic.QuittingRootContinuation
 import GameTheory.Concepts.Stochastic.QuittingRootPerturbation
 import GameTheory.Concepts.Stochastic.QuittingSimpleBranches
 import GameTheory.Concepts.Stochastic.QuittingFirstBranch
+import GameTheory.Concepts.Stochastic.QuittingFirstStageAdapter
 import GameTheory.Concepts.Stochastic.QuittingFiniteHorizonBridge
 import Math.NonnegativeLeadingSymbol
 import Math.Probability.FiniteDiscountedFlow
