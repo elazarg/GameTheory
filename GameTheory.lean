16e288dd9a6a0bc5f05a7b02250beb957cbd9393
Elazar Gershuni <elazarg@gmail.com>
2026-08-02 19:32:17 +0300

    Route quitting root successor certificates

--- GameTheory.lean
+++ GameTheory.lean
@@ -425,6 +425,7 @@ import GameTheory.Concepts.Stochastic.QuittingLiveMass
 import GameTheory.Concepts.Stochastic.QuittingLiveMassRecurrence
 import GameTheory.Concepts.Stochastic.QuittingStationaryLiveMass
 import GameTheory.Concepts.Stochastic.QuittingStationaryPayoff
+import GameTheory.Concepts.Stochastic.QuittingRootSuccessorCertificate
 import GameTheory.Concepts.Stochastic.QuittingOpponentLiveMass
 import GameTheory.Concepts.Stochastic.QuittingLiveTail
 import GameTheory.Concepts.Stochastic.QuittingNearSureRoot
