81aec6c8e87cc94af70f511dc745412898667b7d
Elazar Gershuni <elazarg@gmail.com>
2026-08-03 13:25:24 +0300

    Assemble weighted security floors into uniform equilibrium

--- GameTheory.lean
+++ GameTheory.lean
@@ -120,6 +120,7 @@ import GameTheory.Concepts.Stochastic.TransitionIndependent
 import GameTheory.Concepts.Stochastic.Discounted
 import GameTheory.Concepts.Stochastic.Adaptive
 import GameTheory.Concepts.Stochastic.AdaptiveCertificate
+import GameTheory.Concepts.Stochastic.WeightedSecurityWelfareAssembly
 import GameTheory.Concepts.Stochastic.PublicPhaseCertificate
 import GameTheory.Concepts.Stochastic.AdaptivePotentialSystem
 import GameTheory.Concepts.Stochastic.AdaptiveCertificateTargetPerturbation
