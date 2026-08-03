01c2f699c78f2c6053d55ae3ec1853bb78fe2ac3
Elazar Gershuni <elazarg@gmail.com>
2026-08-03 09:03:50 +0300

    Run marked fences on minimum-debt chains

--- GameTheory.lean
+++ GameTheory.lean
@@ -459,6 +459,7 @@ import GameTheory.Concepts.Stochastic.QuittingNearSureProfile
 import GameTheory.Concepts.Stochastic.QuittingFiniteHorizonBridge
 import GameTheory.Concepts.Stochastic.QuittingMarkedFencePacket
 import GameTheory.Concepts.Stochastic.QuittingMarkedFenceIteration
+import GameTheory.Concepts.Stochastic.QuittingMinDebtMarkedFence
 import GameTheory.Concepts.Stochastic.QuittingMarkedSameNodeCycleRegression
 import GameTheory.Concepts.Stochastic.QuittingMarkedTimeAdvance
 import GameTheory.Concepts.Stochastic.QuittingOpponentClockDichotomy
