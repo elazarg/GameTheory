77f8d7fbc25857e6d371634680b961e6e43f7d8a
Elazar Gershuni <elazarg@gmail.com>
2026-08-03 08:04:51 +0300

    Construct infinite compact serial chains

--- GameTheory.lean
+++ GameTheory.lean
@@ -433,6 +433,7 @@ import GameTheory.Concepts.Stochastic.QuittingLiveTail
 import GameTheory.Concepts.Stochastic.QuittingExceptionalHazard
 import GameTheory.Concepts.Stochastic.QuittingBellmanTelescope
 import GameTheory.Concepts.Stochastic.GraphDirectedCompactPullback
+import GameTheory.Concepts.Stochastic.CompactSerialRelation
 import GameTheory.Concepts.Stochastic.QuittingBlockSurvival
 import GameTheory.Concepts.Stochastic.QuittingInfinitePathCompiler
 import GameTheory.Concepts.Stochastic.QuittingExceptionalBellmanTail
