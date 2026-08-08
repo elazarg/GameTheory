df378684c3396c120bfcd0bd2bf6d985c272f844
Elazar Gershuni <elazarg@gmail.com>
2026-08-09 02:44:32 +0300

    Add multivalued Essential APS umbrella

---
+++ UniformEquilibrium/Quitting/EssentialAPS/Multivalued/All.lean
@@ -0,0 +1,16 @@
+/-
+Copyright (c) 2026 GameTheory contributors. All rights reserved.
+Released under the MIT license as described in the file LICENSE.
+Authors: GameTheory contributors
+-/
+
+import UniformEquilibrium.Quitting.EssentialAPS.Multivalued.SCCExecution
+import UniformEquilibrium.Quitting.EssentialAPS.Multivalued.OccupationRegression
+
+/-!
+# Multivalued essential-APS SCC execution
+
+Public surface for the finite-relation execution trichotomy, the Flesch-SCC
+adapter, component-local charge bounds, and the regression separating global
+occupation balance from a single chronological recurrent path.
+-/
